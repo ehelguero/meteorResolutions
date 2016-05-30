9fb848fff4c0af2424668a4826c8e112f65867ad
loupgarou81 <leloupgarou81@gmail.com>
2016-05-30 19:10:26 +0100

    First commit with react components and mongo

---
+++ imports/ui/App.jsx
@@ -0,0 +1,38 @@
+import React, { Component, PropTypes } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+
+import { Tasks } from '../api/tasks';
+
+import Task from './Task.jsx';
+
+class App extends Component {
+  renderTask() {
+    return this.props.tasks.map((task) => (
+      <Task key={task._id} task={task} />
+    ));
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Todo List</h1>
+        </header>
+
+        <ul>
+          {this.renderTask()}
+        </ul>
+      </div>
+    )
+  }
+}
+
+App.propTypes = {
+  tasks: PropTypes.array.isRequired
+}
+
+export default createContainer(() => {
+  return {
+    tasks: Tasks.find({}).fetch(),
+  };
+}, App)
