236fe5879e7a476204bf63c0c628fadb21cd2bf6
loupgarou81 <leloupgarou81@gmail.com>
2016-06-01 09:45:15 +0100

    Methods added to the api

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -1,3 +1,35 @@
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
 
 export const Tasks = new Mongo.Collection('tasks');
+
+// methods to insert, update and delete tasks
+Meteor.methods({
+  'tasks.insert'(text) {
+    // check if te param is a String
+    check(text, String);
+
+    if (! this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+      owner: this.userId,
+      username: Meteor.users.findOne(this.userId).username,
+    });
+  },
+  'tasks.remove'(taskId) {
+    check(taskId, String);
+
+    Tasks.remove(taskId);
+  },
+  'tasks.update'(taskId, setChecked) {
+    check(taskId, String);
+    check(setChecked, Boolean);
+
+    Tasks.update(taskId, { $set: { checked: setChecked } });
+  },
+})
