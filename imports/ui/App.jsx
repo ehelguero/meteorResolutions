6c64bcdef6a45dbe2c322c294b82f94540ffac15
loupgarou81 <leloupgarou81@gmail.com>
2016-06-01 09:36:11 +0100

    Remove insecure package, add delete and create tasks

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,12 +1,21 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
 
 import { Tasks } from '../api/tasks';
-
 import Task from './Task.jsx';
+import AccountsUIWrapper from './AccountsUIWrapper';
 
 class App extends Component {
+  constructor(props){
+      super(props)
+
+      this.state = {
+        hideCompleted: false
+      };
+  }
+
   handleSubmit(event) {
       event.preventDefault();
       const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
@@ -14,45 +23,75 @@ class App extends Component {
       Tasks.insert({
         text,
         createdAt: new Date(),
+        owner: Meteor.userId(),
+        username: Meteor.user().username,
       });
 
       ReactDOM.findDOMNode(this.refs.textInput).value = '';
   }
 
   renderTask() {
-    return this.props.tasks.map((task) => (
+    let filteredTasks = this.props.tasks;
+    if(this.state.hideCompleted){
+      filteredTasks = filteredTasks.filter( task => !task.checked );
+    }
+    return filteredTasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
+  }
+
+  toggleHideCompleted(){
+    this.setState({ hideCompleted: !this.state.hideCompleted })
   }
 
   render() {
     return (
       <div className="container">
         <header>
-          <h1>Todo List</h1>
-          <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
-            <input
-              type="text"
-              ref="textInput"
-              placeholder="Type to add new tasks"
+          <h1>Todo List {this.props.incompleteCount}</h1>
+          <label className="hide-completed">
+            <input type="checkbox"
+              readOnly
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted.bind(this)}
             />
-          </form>
+          Hide completed task
+          </label>
+          <AccountsUIWrapper />
+          {this.props.currentUser ?
+            <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+              <input
+                className="form-control"
+                type="text"
+                ref="textInput"
+                placeholder="Type to add new tasks"
+              />
+          </form> : ''
+          }
         </header>
 
-        <ul>
-          {this.renderTask()}
-        </ul>
+        <div className="row">
+          <div className="col-md-12">
+            <ul>
+              {this.renderTask()}
+            </ul>
+          </div>
+        </div>
       </div>
     )
   }
 }
 
 App.propTypes = {
-  tasks: PropTypes.array.isRequired
+  tasks: PropTypes.array.isRequired,
+  incompleteCount: PropTypes.number.isRequired,
+  currentUser: PropTypes.object,
 }
 
 export default createContainer(() => {
   return {
-    tasks: Tasks.find({}).fetch(),
+    tasks: Tasks.find({}, {sort: { createdAt: -1 } }).fetch(),
+    incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
+    currentUser: Meteor.user(),
   };
 }, App)
--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,9 +1,33 @@
 import React, { Component, PropTypes } from 'react';
 
+import { Tasks } from '../api/tasks';
+
 export default class Task extends Component {
+  // function to check resolutions
+  toggleChecked() {
+      // use api to update them and mark as the oposite of checked
+      Tasks.update(this.props.task._id, {
+        $set: { checked: !this.props.task.checked },
+      });
+  }
+
+  deleteThisTask() {
+    Tasks.remove(this.props.task._id);
+  }
+
   render() {
+    const taskClassName = this.props.task.checked ? 'checked' : '';
+
     return (
-      <li>{this.props.task.text}</li>
+      <li className={taskClassName}>
+        <button className='btn btn-default delete' onClick={this.deleteThisTask.bind(this)} >&times;</button>
+        <input type="checkbox"
+          readOnly
+          checked={this.props.task.checked}
+          onClick={this.toggleChecked.bind(this)}
+        />
+      {this.props.task.text} - {this.props.task.username}
+      </li>
     );
   }
 }
